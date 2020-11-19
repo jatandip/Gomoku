aafd0bc57754baf5cce1414d4a2b4e0308b66f38
kishan151999 <kishanp1599@gmail.com>
2020-11-18 21:26:48 -0800

    "updated bot features"

--- Game_Bot.py
+++ Game_Bot.py
@@ -107,6 +107,126 @@ class Game_Bot:
             ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
         ]
 
+        blockPlayerBoard = [
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']]
+
+        for i in range(len(self.gameBoard)):
+            for j in range(len(self.gameBoard[i])):
+                points = [0, 0, 0, 0, 0, 0, 0, 0]
+
+                stop = False
+                horRightMove = j + 1
+                while horRightMove < 19 and stop != True and points[0] < 5 and self.gameBoard[i][horRightMove] == "b":
+                    points[0] += 1
+                    if self.gameBoard[i][horRightMove] == "w":
+                        stop = True
+                    horRightMove += 1
+
+                stop = False
+                horLeftMove = j - 1
+                while horLeftMove >= 0 and stop != True and points[1] < 5 and self.gameBoard[i][horLeftMove] == "b":
+                    points[1] += 1
+                    if self.gameBoard[i][horLeftMove] == "w":
+                        stop = True
+                    horLeftMove -= 1
+
+                stop = False
+                verTopMove = i - 1
+                while verTopMove >= 0 and stop != True and points[2] < 5 and self.gameBoard[verTopMove][j] == "b":
+                    points[2] += 1
+                    if self.gameBoard[verTopMove][j] == "w":
+                        stop = True
+                    verTopMove -= 1
+
+                stop = False
+                verBottomMove = i + 1
+                while verBottomMove < 19 and stop != True and points[3] < 5 and self.gameBoard[verBottomMove][j] == "b":
+                    points[3] += 1
+                    if self.gameBoard[verBottomMove][j] == "w":
+                        stop = True
+                    verBottomMove += 1
+
+                stop = False
+                diaTopRightX = i - 1
+                diaTopRightY = j + 1
+                while diaTopRightX >= 0 and diaTopRightY < 19 and stop != True and points[4] < 5 and \
+                        self.gameBoard[diaTopRightX][diaTopRightY] == "b":
+                    points[4] += 1
+                    if self.gameBoard[diaTopRightX][diaTopRightY] == "w":
+                        stop = True
+                    diaTopRightX -= 1
+                    diaTopRightY += 1
+
+                stop = False
+                diaTopLeftX = i - 1
+                diaTopLeftY = j - 1
+                while diaTopLeftX >= 0 and diaTopLeftY >= 0 and stop != True and points[5] < 5 and \
+                        self.gameBoard[diaTopLeftX][diaTopLeftY] == "b":
+                    points[5] += 1
+                    if self.gameBoard[diaTopLeftX][diaTopLeftY] == "w":
+                        stop = True
+                    diaTopLeftX -= 1
+                    diaTopLeftY -= 1
+
+                stop = False
+                diaBottomRightX = i + 1
+                diaBottomRightY = j + 1
+                while diaBottomRightX < 19 and diaBottomRightY < 19 and stop != True and points[6] < 5 and \
+                        self.gameBoard[diaBottomRightX][diaBottomRightY] == "b":
+                    points[6] += 1
+                    if self.gameBoard[diaBottomRightX][diaBottomRightY] == "w":
+                        stop = True
+                    diaBottomRightX += 1
+                    diaBottomRightY += 1
+
+                stop = False
+                diaBottomLeftX = i + 1
+                diaBottomLeftY = j - 1
+                while diaBottomLeftX < 19 and diaBottomLeftY >= 0 and stop != True and points[7] < 5 and \
+                        self.gameBoard[diaBottomLeftX][diaBottomLeftY] == "b":
+                    points[7] += 1
+                    if self.gameBoard[diaBottomLeftX][diaBottomLeftY] == "w":
+                        stop = True
+                    diaBottomLeftX += 1
+                    diaBottomLeftY -= 1
+
+                blockPlayerBoard[i][j] = str(max(points))
+
+        bestScore = 0
+        bestPos = []
+        for x in range(len(blockPlayerBoard)):
+            for y in range(len(blockPlayerBoard[x])):
+                if int(blockPlayerBoard[x][y]) >= bestScore and self.gameBoard[x][y] != "b" and self.gameBoard[x][
+                    y] != "w":
+                    bestScore = int(blockPlayerBoard[x][y])
+                    bestPos = [x, y]
+
+        if bestScore == 4:
+            self.gameBoard[bestPos[0]][bestPos[1]] = "w"
+            print("Blocked")
+            print('\n'.join(map(''.join, self.gameBoard)))
+            return "None"
+        
+
+
         for i in range(len(self.gameBoard)):
             for j in range(len(self.gameBoard[i])):
                 points = [0, 0, 0, 0, 0, 0, 0, 0]
